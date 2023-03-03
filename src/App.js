06764636241d08d6b857e1c6fc2177781663992e
SAM.AM <samir.amri@gmail.com>
2023-03-03 16:03:22 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import PostList from "./Components/PostsList";
+import "./styles.css";
+export default function App() {
+  return (
+    <main>
+      <PostList />
+    </main>
+  );
+}
